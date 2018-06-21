6c7a7f298b48cb699faa403278579dbeee1c1ad9
LAPTOP-P9CV61TQ\leasc <Lea.Scholtes@htwg-konstanz.de>
2018-06-21 12:31:52 +0200

    Feature Seite überarbeitet mit neuen Bildern

--- resources/views/mio_features.blade.php
+++ resources/views/mio_features.blade.php
@@ -23,12 +23,32 @@
                     </div>
                 </div>
                 <div class="col-lg-6 my-about-padding">
-                    <img src="/BilderfuerFeature/Doors.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfDoors">
+                    <img src="/PictureFeature/Doors.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfDoors">
                 </div>
             </div>
         </div>
     </div>
     <hr>
+
+    <!-- Two Column Section Doors-->
+    <div class="col-lg-10 offset-lg-1 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 my-about-padding">
+                    <div class="my-put-things-in-vertically-and-horizontally-center text-center">
+                        <p>If you want to add a new door you get this view where you can give it a name and and you have to fill in the id of your device.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 my-about-padding">
+                    <img src="/PictureFeature/Add_Door.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfAddDoors">
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+
+
+
 
     <!-- Two Column Section menu -->
     <div class="col-lg-10 offset-lg-1 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
@@ -36,11 +56,11 @@
             <div class="row">
                 <div class="col-lg-6 my-about-padding">
                     <div class="my-put-things-in-vertically-and-horizontally-center text-center">
-                        <p>In menu you can open your door, manage rights for users or see the transactions in a protocol.</p>
+                        <p>In menu you can open your door, manage rights for users, add users or see the transactions in a protocol. Indeed you can't see the buttons for manage rights and add user if you are a user and not a client. At the top you see the chosen door.</p>
                     </div>
                 </div>
                 <div class="col-lg-6 my-about-padding">
-                    <img src="/BilderfuerFeature/Menu.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfMenu">
+                    <img src="/PictureFeature/Manage_Door.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfMenu">
                 </div>
             </div>
         </div>
@@ -55,16 +75,34 @@
                 <div class="col-lg-6 my-about-padding">
                     <div class="my-put-things-in-vertically-and-horizontally-center text-center">
                         <p>If you choose "see Transactions" you get a view about who opened the door at which time.
-                        You can also search for an user or a date.</p>
+                        You can also search for an user or a date. If you don't have the protocol right you can see your entries only.</p>
                     </div>
                 </div>
                 <div class="col-lg-6 my-about-padding">
-                    <img src="/BilderfuerFeature/Protocol.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfProtocol">
+                    <img src="/PictureFeature/Transactions.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfProtocol">
                 </div>
             </div>
         </div>
     </div>
     <hr>
+
+    <!-- Two Column Section AccessRight -->
+    <div class="col-lg-10 offset-lg-1 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 my-about-padding">
+                    <div class="my-put-things-in-vertically-and-horizontally-center text-center">
+                        <p>If you choose "add User" you can add an user to the chosen door. Generally the new user gets all rights.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 my-about-padding">
+                    <img src="/PictureFeature/Add_User.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfAddUser">
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+
 
     <!-- Two Column Section user -->
     <div class="col-lg-10 offset-lg-1 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
@@ -73,11 +111,11 @@
                 <div class="col-lg-6 my-about-padding">
                     <div class="my-put-things-in-vertically-and-horizontally-center text-center">
                         <p>If you choose "manage Rights" you get an overwiew about all users who have access to this door.
-                        Then you can select one or add a new user.</p>
+                        Then you can select one.</p>
                     </div>
                 </div>
                 <div class="col-lg-6 my-about-padding">
-                    <img src="/BilderfuerFeature/Benutzer.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfUsers">
+                    <img src="/PictureFeature/Select_User.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfUsers">
                 </div>
             </div>
         </div>
@@ -94,7 +132,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6 my-about-padding">
-                    <img src="/BilderfuerFeature/Rights.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfRight">
+                    <img src="/PictureFeature/Rights.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfRight">
                 </div>
             </div>
         </div>
@@ -107,11 +145,11 @@
             <div class="row">
                 <div class="col-lg-6 my-about-padding">
                     <div class="my-put-things-in-vertically-and-horizontally-center text-center">
-                        <p>If you give the user the Access Right you can pick the days you give him access and the time.</p>
+                        <p>If you give the user the Access Right you can pick the days and the time you give him access.</p>
                     </div>
                 </div>
                 <div class="col-lg-6 my-about-padding">
-                    <img src="/BilderfuerFeature/Rechte.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfAccessRight">
+                    <img src="/PictureFeature/Rights_Ext.jpg" class="img-fluid rounded my-put-things-in-vertically-and-horizontally-center" alt="PictureOfAccessRight">
                 </div>
             </div>
         </div>
